503430c38a2a900d72f541a17c99b3865f982541
kotmilkmeowtwo <forgithubkotmilkmeow@gmail.com>
2022-05-29 21:47:04 +0300

    ня

---
+++ src/main/java/ru/meowland/TowerDefense.java
@@ -0,0 +1,12 @@
+package ru.meowland;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+
+public class TowerDefense extends Plugin{
+    public static void main(String[] args) {
+
+    }
+}
